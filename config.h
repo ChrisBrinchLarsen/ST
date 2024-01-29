48615e21e3600555fdf86e85388e78a07dbe7977
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2024-01-29 16:27:09 +0100

    Custom Color Scheme

--- config.h
+++ config.h
@@ -100,32 +100,59 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.85, alphaUnfocused = 0.5;
+float alpha = 0.93, alphaUnfocused = 0.4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
     [0] = "#000000", /* black   */
-    [1] = "#ff5555", /* red     */
-    [2] = "#50fa7b", /* green   */
-    [3] = "#f1fa8c", /* yellow  */
-    [4] = "#bd93f9", /* blue    */
-    [5] = "#ff79c6", /* magenta */
-    [6] = "#8be9fd", /* cyan    */
+
+    //[1] = "#ff5555", /* red     */
+    [1] = "#FF2E43", /* My red     */
+
+    //[2] = "#50fa7b", /* green   */
+    [2] = "#2EFF4D", /* My green   */
+
+    //[3] = "#f1fa8c", /* yellow  */
+    [3] = "#ffc02e", /* My yellow  */
+
+    //[4] = "#bd93f9", /* blue    */
+    [4] = "#30ffa4", /* My blue    */
+
+    //[5] = "#ff79c6", /* magenta */
+    [5] = "#a12eff", /* My magenta */
+
+    //[6] = "#8be9fd", /* cyan    */
+    [6] = "#00ada7", /* My cyan    */
+
     [7] = "#bbbbbb", /* white   */
                                   
     /* 8 bright colors */
     [8]  = "#44475a", /* black   */
-    [9]  = "#ff5555", /* red     */
-    [10] = "#50fa7b", /* green   */
-    [11] = "#f1fa8c", /* yellow  */
-    [12] = "#bd93f9", /* blue    */
-    [13] = "#ff79c6", /* magenta */
-    [14] = "#8be9fd", /* cyan    */
+
+    //[9]  = "#ff5555", /* red     */
+    [9] = "#FF2E43", /* My red     */
+
+    //[10] = "#50fa7b", /* green   */
+    [10] = "#2EFF4D", /* My green   */
+
+    //[11] = "#f1fa8c", /* yellow  */
+    [11] = "#ffc02e", /* My yellow  */
+
+    //[12] = "#bd93f9", /* blue    */
+    [12] = "#30ffa4", /* My blue    */
+
+    //[13] = "#ff79c6", /* magenta */
+    [13] = "#a12eff", /* My magenta */
+
+    //[14] = "#8be9fd", /* cyan    */
+    [14] = "#00ada7", /* My cyan    */
+
     [15] = "#ffffff", /* white   */
                                    
     /* special colors */
-    [256] = "#282a36", /* background */
+    //[256] = "#282a36", /* background */
+    [256] = "#031624", // My background
     [257] = "#f8f8f2", /* foreground */
 
 	/* more colors can be added after 255 to use with DefaultXX */
@@ -144,7 +171,7 @@ unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
-unsigned int bg = 261, bgUnfocused = 261;
+unsigned int bg = 256, bgUnfocused = 256;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
